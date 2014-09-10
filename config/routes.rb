0c8113df4ae8339be7363eddd0db8b852579ae6e
timproctor <timandcamiproctor@gmail.com>
2014-09-10 14:34:20 -0600

    saved to merge with master

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :categories, only: [:index]
+  resources :categories, only: :show
   resources :items, only: [:show]
   resources :users, only: [:new, :create]
   resource :account, only: [:show, :edit, :update]
