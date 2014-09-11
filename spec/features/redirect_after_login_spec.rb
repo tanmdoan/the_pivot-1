d061e83daa2038e54100e48362d4185ce912f396
Andy Mention <amention@gmail.com>
2014-09-11 13:54:47 -0600

    Redirect to login path

--- spec/features/redirect_after_login_spec.rb
+++ spec/features/redirect_after_login_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 describe 'Redirect after login', type: :feature do
   context 'After accessing a protected page' do
-    xit 'returns to the referring page' do
+    it 'returns to the referring page' do
       register_as_borrower
       click_link 'Logout'
 
-      visit borrower_orders_path
+      visit borrower_loans_path
       fill_in 'Email', with: 'yourdad123@aol.com'
       fill_in 'Password', with: '123'
       click_button 'Login'
-      expect(current_path).to eq borrower_orders_path
+      expect(current_path).to eq borrower_loans_path
     end
   end
 
   context 'From an unprotected page' do
-    xit 'redirects to order dashboard' do
+    it 'redirects to borrower dashboard' do
       register_as_borrower
       login_as_borrower
 
-      expect(current_path).to eq borrower_orders_path
+      expect(current_path).to eq borrower_path
     end
   end
 end
