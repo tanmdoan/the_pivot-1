136b007ff5a7e30a976aa48e6fe569da9bd849c2
Andy Mention <amention@gmail.com>
2014-09-11 10:41:24 -0600

    Auth tests passing

--- spec/features/authorization_spec.rb
+++ spec/features/authorization_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'Authorization', type: :feature do
   context 'As a guest' do
-    xit 'cannot view a page requiring authorization' do
-      visit borrower_orders_path
+    it 'cannot view a page requiring authorization' do
+      visit borrower_path
 
       expect(current_path).to eq login_path
       expect(page).to have_content 'You must be logged in to access that.'
@@ -11,12 +11,15 @@ describe 'Authorization', type: :feature do
   end
 
   context 'As a borrower' do
-    xit 'can view a page requiring authorization' do
+    it 'can view a page requiring authorization' do
       register_as_borrower
       login_as_borrower
 
-      visit borrower_orders_path
-      expect(current_path).to eq borrower_orders_path
+      visit borrower_path
+      expect(current_path).to eq borrower_path
     end
+  end
+
+  xit "needs more tests?" do
   end
 end
