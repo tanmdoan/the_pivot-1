900997ddbb3998f9b2b9d55dea75a9dc1134b600
Andy <amention@gmail.com>
2014-09-10 22:26:48 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -64,15 +64,6 @@ ActiveRecord::Schema.define(version: 20140911014714) do
     t.integer  "user_id"
   end
 
-  create_table "order_items", force: true do |t|
-    t.integer  "item_id"
-    t.integer  "quantity"
-    t.integer  "unit_price"
-    t.integer  "order_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "order_loans", force: true do |t|
     t.integer  "loan_id"
     t.integer  "quantity"
