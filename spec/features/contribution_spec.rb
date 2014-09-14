e4017b6f26c3d9812b316dd4f980516c39b87b28
Andy Mention <amention@gmail.com>
2014-09-14 13:03:04 -0600

    Start carts controller a-movin'

--- spec/features/contribution_spec.rb
+++ spec/features/contribution_spec.rb
@@ -3,17 +3,31 @@ require 'rails_helper'
 describe 'when viewing the loans' do
 
 	context 'as a lender' do
-		let(:loan) { Loan.create(title: 'Cow1', description: 'Clearly, the best cow', amount: 4300) }
+		let(:loan) { Loan.create(id: 1,
+														title: 'Buy a cow',
+														description: 'Need to buy a milking cow for our farm',
+														amount: 50000,
+														requested_by: "2014-09-10 13:43:00 -0600",
+														repayments_begin: "2014-09-10 13:43:00 -0600",
+														monthly_payment: 100000,
+														user_id: 1
+														) }
 
 		before(:each) do
 			register_and_login_as_lender
       lender = User.last
-      contribution = Contribution.create(user_id: lender.id, loan_id: 1, amount: 25)
-      loan
-      loan_contribution = LoanContribution.create(contribution_id: contribution.id, loan_id: loan.id)
+			loan
+      # contribution = Contribution.create(user_id: lender.id, loan_id: 1, amount: 25)
+			category = Category.create!(id: 1, name: "Agriculture")
+      LoanCategory.create!(loan_id: 1, category_id: 1)
+      # loan_contribution = LoanContribution.create(contribution_id: contribution.id, loan_id: loan.id)
 		end
 
-		it 'can choose a loan and make a contribution to it' do
+		it 'can make a contribution to a loan' do
+			visit categories_path
+			click_on "Loan Now"
+
+			save_and_open_page
 		end
 
 		xit 'can edit an order' do
