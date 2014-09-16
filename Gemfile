3eb391aac45659fea58c1169c0c0764779713e15
timproctor <timandcamiproctor@gmail.com>
2014-09-15 18:25:38 -0600

    Installed Ransack gem for searching, Refactored category-index-view into partial, Activated basic title and description search functionality

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'stripe'
 gem 'heroku_secrets', github: 'alexpeattie/heroku_secrets'
 gem 'pg'
 gem 'aasm'
+gem 'ransack'
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -4,7 +4,24 @@ class LoansController < ApplicationController
 	end
 
 	def index
-		@loans = loans
+		@categories = Category.all.decorate
+		@q = Loan.search(params[:q])
+		if params[:q]
+			@loans = @q.result.includes(:categories).all
+		else
+		  @loans = Loan.includes(:categories).all
+	  end
+
+		@loans = @loans.decorate
+
+		# group by category
+		@loans_by_category = {}
+		@loans.each do |loan|
+			loan.categories.each do |category|
+				@loans_by_category[category.name] ||= []
+				@loans_by_category[category.name] << loan
+			end
+		end
 	end
 
 	private
